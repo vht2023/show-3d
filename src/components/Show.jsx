096eee8e9723a1c9ba2e2295d7864dd2ca16fd22
Thien Vo <thien.vh21@gmail.com>
2023-07-27 11:05:03 +0700

    test

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,6 +1,8 @@
 import { Environment, OrbitControls, Sky, useTexture } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
-import MyAvatar from "./MyAvatar";
+import MyModel from "./MyModel";
+import Lamborghini from "./Lamborghini";
+import CanhModel from "./CanhModel";
 
 export const Show = () => {
     const texture = useTexture("textures/bg1.jpg");
@@ -10,9 +12,19 @@ export const Show = () => {
             <OrbitControls />
             <Sky />
             <Environment preset="sunset" />
-            <group position-y={-1} position-x={0} position-z={1}>
-                <MyAvatar />
+            <group position-y={-1} position-x={-2} position-z={1}>
+                <MyModel />
             </group>
+            <group position-y={-1} position-x={0} position-z={-1}>
+                <CanhModel />
+            </group>
+            <group position-y={-1} position-x={3} position-z={1}>
+                <Lamborghini />
+            </group>
+            <mesh scale={[0.5, 1, 0.5]} position-y={-1} position-z={-1}>
+                <boxGeometry />
+                <meshStandardMaterial color="#a5a5a5" />
+            </mesh>
             <mesh scale={[18.2, 0.5, 7]} position-y={-1.25} position-z={1}>
                 <boxGeometry />
                 <meshStandardMaterial color="white" />
